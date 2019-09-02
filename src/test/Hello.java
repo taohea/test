7e4bd1658b75c7ea684cb63bb810d61dd3df80c1
taohea <513493061@qq.com>
2019-09-02 11:08:24 +0800

    Update Hello.java

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -9,6 +9,8 @@ public class Hello {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
